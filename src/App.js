347fadd0578a90dc934ec9e28e36d405e9f1aadf
bhuwan <bkspoudel@gmail.com>
2022-05-18 21:07:56 +0200

    created 9 buttons on the board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Heading from "./components/Heading";
+import Board from "./components/Board";
 
 function App() {
   return (
     <div className="App">
       <h1>Game Board</h1>
       <Heading />
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./board.css";
+
+function Board() {
+  return (
+    <div className="board">
+      <table>
+        <tr>
+          <button></button>
+          <button></button>
+          <button></button>
+        </tr>
+        <tr>
+          <button></button>
+          <button></button>
+          <button></button>
+        </tr>
+        <tr>
+          <button></button>
+          <button></button>
+          <button></button>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default Board;
