6bcf43e0e0686ade1cbbe68eba313943745737ae
bhuwan <bkspoudel@gmail.com>
2022-05-18 21:02:27 +0200

    created component Heading to display the turn of the player

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Heading from "./components/Heading";
 
 function App() {
   return (
     <div className="App">
       <h1>Game Board</h1>
+      <Heading />
     </div>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div>
+      <h3>Player 1's Turn...</h3>
+    </div>
+  );
+}
+
+export default Heading;
